f1e68d61313ffa4424d0a6af48ec32d307c6730c
Ralph <Ralph90062@gmail.com>
2022-07-21 15:23:37 -0700

    add opencv with camera

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+import cv2
+from flask import Flask, render_template, Response
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -10,6 +25,11 @@ def index():
 @app.route('/camera_feed')
 def camera_feed():
     return render_template('camera_feed.html')
+
+
+@app.route('/video_camera')
+def video_camera():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
