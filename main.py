f82485feeb8b3b5777bbf21ceaec68a4e3cc0fe6
Ralph <Ralph90062@gmail.com>
2022-07-27 10:57:48 -0700

    Purple Dots

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
+import cvzone
+import numpy as np
 from flask import Flask, render_template, Response
 app = Flask(__name__)
 
@@ -26,7 +28,19 @@ def hand_tracking(camera_frame):
                 h, w, c = camera_frame.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(id, cx, cy)
-                if id == 0:
+                if id == 4:
+                    cv2.circle(camera_frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+                if id == 8:
+                    cv2.circle(camera_frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+                if id == 12:
+                    cv2.circle(camera_frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+                if id == 16:
+                    cv2.circle(camera_frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+                if id == 20:
                     cv2.circle(camera_frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(camera_frame, handLms, mpHands.HAND_CONNECTIONS)
@@ -47,6 +61,17 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def OverlayImages():
+    imgBack = cv2.imread("static/images/GuitarFrets.jpg")
+    imgFront = cv2.imread("static/images/GuitarFrets.jpg", cv2.IMREAD_UNCHANGED)
+
+    imgBack[0:300, 0:300] = imgFront
+
+    imgResult = cvzone.overlayPNG(imgBack, imgFront, [20, 20])
+
+    cv2.imshow("Image", imgResult)
+    cv2.waitKey(0)
 
 
 @app.route('/')
